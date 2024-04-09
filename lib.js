72e423980ba5d6838523c6950d6e7956b1a33b04
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2024-04-09 19:48:39 +0300

    register,login,logout handlers

--- lib.js
+++ lib.js
@@ -1,14 +1,18 @@
-import {html,render as renderBase} from './node_modules/lit-html/lit-html.js';
-import page from './node_modules/page/page.mjs';
+import { html, render as renderBase} from '../node_modules/lit-html/lit-html.js';
+import { classMap } from '../node_modules/lit-html/directives/class-map.js';
+import { styleMap } from '../node_modules/lit-html/directives/style-map.js';
+import page from '../node_modules/page/page.mjs';
 
-const root=document.querySelector('main');
+const root = document.querySelector('main');
 
-function render(template) {
-    renderBase(template,root);
+function render(templateResult) {
+    renderBase(templateResult,root);
 }
 
 export {
     html,
-    page,
-    render
+    render,
+    classMap,
+    styleMap,
+    page
 }
