3e73d6f36add088026942de1c1b43fc0876fb789
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2024-04-09 12:14:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { catalogRender, detailsRender } from './catalog.js';
-import {page} from './lib.js';
+import { page } from './lib.js';
 
-page('/',catalogRender);
-page('/details/:id',detailsRender);
+page('/', catalogRender);
+page('/details/:id', detailsRender);
 page.start();
