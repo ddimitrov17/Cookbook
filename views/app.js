87016fc29939cfdf44192646e7db0daafaabe656
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2024-04-09 20:02:55 +0300

    update

--- views/app.js
+++ views/app.js
@@ -1,5 +1,7 @@
 import { catalogRender, detailsRender } from './catalog.js';
 import { page } from '../lib.js';
+import { showRegister } from './register.js';
 
 page('/', catalogRender);
+page('/register',showRegister);
 page.start();
---
+++ views/register.js
@@ -0,0 +1,16 @@
+import {html,render} from '../lib.js';
+
+const registerTemplate=() => html`
+        <article>
+            <h2>Register</h2>
+            <form>
+                <label>E-mail: <input type="text" name="email"></label>
+                <label>Password: <input type="password" name="password"></label>
+                <label>Repeat: <input type="password" name="rePass"></label>
+                <input type="submit" value="Register">
+            </form>
+        </article>`;
+
+export function showRegister() {
+    render(registerTemplate());
+}
